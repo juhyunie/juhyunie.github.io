77fd2d2e056438d197f9ee84a7795b2c7a3b2379
Jackie Kim <kimj761@newschool.edu>
2017-05-05 09:53:54 -0400

    update

---
+++ Studio/finalblog/assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$('#fullpage').fullpage({
+		sectionsColor: ['white', '#0052E2', 
+		'black'],
+		keyboardScrolling: true,
+		scrollingSpeed: 800,
+		controlArrows: true,
+
+		
+	});
+});
